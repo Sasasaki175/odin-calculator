afd8dfeb0533bf194dad89019ad257ed9533df43
Sasasaki175 <207643966+Sasasaki175@users.noreply.github.com>
2025-06-17 09:07:13 +0900

    Add operate function Added operate fucntion that takes an operator and two numbers and then calls one of the above functions on the numbers.

--- script.js
+++ script.js
@@ -14,6 +14,24 @@ function divide(a, b) {
   return a / b;
 }
 
+function operate(operator, firstNumber, secondNumber) {
+  switch(operator) {
+    case 1:
+      add(firstNumber, secondNumber);
+      break;
+    case 2:
+      subtract(firstNumber, secondNumber);
+      break;
+    case 3:
+      multiply(firstNumber, secondNumber);
+      break;
+    case 4:
+      divide(firstNumber, secondNumber);
+      break;
+  }
+}
+
 let firstNumber;
 let secondNumber;
 let operator;
+
